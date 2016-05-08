ce309847a50d67396191d2a2fc1b2365f19870b9
Lenin Capa <leviskp87@gmail.com>
2016-05-07 22:35:19 -0500

    fix missing params devise register

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,12 +14,10 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_params
-    devise_parameter_sanitizer.for(:sign_up) << :username
-    devise_parameter_sanitizer.for(:account_update).push(:name,
-                                                          :username,
-                                                          :email,
-                                                          :identification,
-                                                          :address,
-                                                          :telephone)
+    added_attrs = [:username, :email, :name]
+    edit_attrs = [:username, :email, :name, :identification, :address, :telephone]
+    devise_parameter_sanitizer.for(:sign_up) << added_attrs
+    devise_parameter_sanitizer.for(:sign_in) << added_attrs
+    devise_parameter_sanitizer.for(:account_update) << edit_attrs
   end
 end
